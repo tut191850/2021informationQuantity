a681349df3482983d29e79c71d9e9f784c524a6e
tut191850 <97152799+tut191850@users.noreply.github.com>
2022-01-18 20:26:51 +0900

    Update Hello.java

--- s4/B191850/Hello.java
+++ s4/B191850/Hello.java
@@ -3,7 +3,7 @@ import java.lang.*;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("github(B191850)");
+        System.out.println("local(B191850)");
     }
 }
 
